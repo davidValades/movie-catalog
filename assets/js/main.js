4b8332719efaff0d11201abf97635c6d0c8df008
david <davidvaladesnavarro@gmail.com>
2026-04-15 14:30:29 +0200

    arreglo de rutas

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,10 +29,14 @@ carousels.forEach((wrapper) => {
 // Si el bloc de notas dice que el tema es 'light', le ponemos la clase al HTML y cambiamos el icono
 if (temaGuardado === "light") {
   document.documentElement.classList.add("light");
-  if (btnTheme) btnTheme.textContent = "🌙";
+  if (btnTheme)
+    btnTheme.innerHTML =
+      '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-moon-icon lucide-moon"><path d="M20.985 12.486a9 9 0 1 1-9.473-9.472c.405-.022.617.46.402.803a6 6 0 0 0 8.268 8.268c.344-.215.825-.004.803.401"/></svg>';
 } else {
   // Si no dice nada o dice 'dark', nos aseguramos de que el botón sea un sol
-  if (btnTheme) btnTheme.textContent = "☀️";
+  if (btnTheme)
+    btnTheme.innerHTML =
+      '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-sun-icon lucide-sun"><circle cx="12" cy="12" r="4"/><path d="M12 2v2"/><path d="M12 20v2"/><path d="m4.93 4.93 1.41 1.41"/><path d="m17.66 17.66 1.41 1.41"/><path d="M2 12h2"/><path d="M20 12h2"/><path d="m6.34 17.66-1.41 1.41"/><path d="m19.07 4.93-1.41 1.41"/></svg>';
 }
 
 // 2. La función que cambia la clase, el icono Y GUARDA en la memoria
@@ -43,10 +47,14 @@ function switchTheme() {
   const esModoClaro = document.documentElement.classList.contains("light");
 
   if (esModoClaro) {
-    if (btnTheme) btnTheme.textContent = "🌙";
+    if (btnTheme)
+      btnTheme.innerHTML =
+        '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-moon-icon lucide-moon"><path d="M20.985 12.486a9 9 0 1 1-9.473-9.472c.405-.022.617.46.402.803a6 6 0 0 0 8.268 8.268c.344-.215.825-.004.803.401"/></svg>';
     localStorage.setItem("miTema", "light"); // 💾 Guardamos en la memoria
   } else {
-    if (btnTheme) btnTheme.textContent = "☀️";
+    if (btnTheme)
+      btnTheme.innerHTML =
+        '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-sun-icon lucide-sun"><circle cx="12" cy="12" r="4"/><path d="M12 2v2"/><path d="M12 20v2"/><path d="m4.93 4.93 1.41 1.41"/><path d="m17.66 17.66 1.41 1.41"/><path d="M2 12h2"/><path d="M20 12h2"/><path d="m6.34 17.66-1.41 1.41"/><path d="m19.07 4.93-1.41 1.41"/></svg>';
     localStorage.setItem("miTema", "dark"); // 💾 Guardamos en la memoria
   }
 }
